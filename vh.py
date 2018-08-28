455dfd563e43ec6637046cd0f0293af8f5b28403
Florin Sarba <florin@dreamproduction.com>
2018-08-28 17:21:03 +0300

    Bumped version

--- vh.py
+++ vh.py
@@ -22,7 +22,7 @@ class VirtualHosts:
     config = None
     skeletons = None
     vhosts = None
-    version = "v1.2.1"
+    version = "v1.2.2"
 
     def __init__(self):
         start = time.time()
